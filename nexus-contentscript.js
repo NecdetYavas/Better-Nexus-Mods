e68148ac2e503e1fc10d4f76aa34c85ea70faaca
Necdet Yavaş <necdet@necdetyavas.com>
2024-07-30 03:39:36 +0300

    Fixed premium banner, removed code related to donation form

--- nexus-contentscript.js
+++ nexus-contentscript.js
@@ -1,7 +1,3 @@
-const premiumBanners = document.getElementsByClassName("premium-banner");
-while (premiumBanners.length > 0) {
-	premiumBanners[0].remove();
-}
 const premiumBlocks = document.getElementsByClassName("premium-block");
 while (premiumBlocks.length > 0) {
 	premiumBlocks[0].remove();
@@ -9,8 +5,4 @@ while (premiumBlocks.length > 0) {
 const collectionsBannerWrappers = document.getElementsByClassName("collections-banner-wrapper");
 while (collectionsBannerWrappers.length > 0) {
 	collectionsBannerWrappers[0].remove();
-}
-const donationForms = document.getElementsByClassName("donation-form");
-while (donationForms.length > 0) {
-	donationForms[0].remove();
 }
