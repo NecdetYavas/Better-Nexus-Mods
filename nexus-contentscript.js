e615b247150ceba0bff9bc090dbd9172f77aac4b
Necdet Yavaş <necdet@necdetyavas.com>
2024-07-30 03:02:57 +0300

    Initial commit

---
+++ nexus-contentscript.js
@@ -0,0 +1,16 @@
+const premiumBanners = document.getElementsByClassName("premium-banner");
+while (premiumBanners.length > 0) {
+	premiumBanners[0].remove();
+}
+const premiumBlocks = document.getElementsByClassName("premium-block");
+while (premiumBlocks.length > 0) {
+	premiumBlocks[0].remove();
+}
+const collectionsBannerWrappers = document.getElementsByClassName("collections-banner-wrapper");
+while (collectionsBannerWrappers.length > 0) {
+	collectionsBannerWrappers[0].remove();
+}
+const donationForms = document.getElementsByClassName("donation-form");
+while (donationForms.length > 0) {
+	donationForms[0].remove();
+}
